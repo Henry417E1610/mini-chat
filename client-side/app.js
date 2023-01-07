719651909a9e4e0767b0087ee6525c0a00bbc4fc
Henry417E1610 <60022442+Henry417E1610@users.noreply.github.com>
2023-01-06 19:01:08 -0700

    added client and server sides

---
+++ client-side/app.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+$('form').on('submit',function () {
+    var text = $('#message').val();
+    var who = $("#initials").val();
+    socket.emit('message', who + ": " + text);
+    $('#message').val('');
+    return false;
+  });
+
+  socket.on('message', function (msg) {
+    $('<li>').text(msg).appendTo('#history');
+  });
